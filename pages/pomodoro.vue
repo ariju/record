ca296971d697e9ced219c7169252abc5187cfed1
ariju <jun.0309ari@gmail.com>
2021-04-21 16:10:55 +0900

    カウントダウンタイマーページの作成

---
+++ pages/pomodoro.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col align="center">
+            <v-card-actions>
+              <v-btn @click="dialog = true" color="secondary" dark small fab
+                ><v-icon>mdi-cog-outline</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <Pomodoro :dialog="dialog" :closeDialog="closeDialog" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Pomodoro from "~/components/Pomodoro";
+
+export default {
+  layout: "main",
+  name: "App",
+
+  components: {
+    Pomodoro
+  },
+
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    closeDialog() {
+      this.dialog = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.v-col {
+  position: relative;
+  margin-top: 30px;
+}
+.v-btn {
+  position: absolute;
+  top: 20px;
+}
+</style>
