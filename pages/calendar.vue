b4f3128857ddd13be3796005a2e9a106bb9879df
ariju <jun.0309ari@gmail.com>
2021-04-21 16:07:34 +0900

    calendarページの作成

---
+++ pages/calendar.vue
@@ -0,0 +1,14 @@
+<template>
+  <Calendar />
+</template>
+
+<script>
+import Calendar from "~/components/Calendar";
+
+export default {
+  layout: "main",
+  components: {
+    Calendar
+  }
+};
+</script>
